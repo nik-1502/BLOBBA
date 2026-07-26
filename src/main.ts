690ab87030e6d5cacc525c5631e136f08bc4ac23
Niklas Lauer <niklas.priv@gmail.com>
2026-07-27 01:18:12 +0200

    Separate Blobben splash centerline

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ import './subpage-theme.css'
 import busfahrerGameImage from './assets/spielbild icons/blobb-fahrer-cover-clean.png'
 import blobbenGameImage from './assets/spielbild icons/blobben-cover-light-transparent.png'
 import busfahrerGameImageNeon from './assets/spielbild icons/blobb-fahrer-cover-neon-chroma-v4.png'
-import blobbenGameImageNeon from './assets/spielbild icons/blobben-cover-neon-connected-v6.png'
+import blobbenGameImageNeon from './assets/spielbild icons/blobben-cover-neon-split-v7.png'
 import heroLogo from './assets/überschrift/blobba-logo-clean-outlined.png'
 import heroLogoNeon from './assets/überschrift/blobba-logo-neon-original.png'
 
