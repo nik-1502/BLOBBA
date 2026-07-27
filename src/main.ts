3eb8148609a5854232c7b2954f6d7fa2ed8aa0c3
Niklas Lauer <niklas.priv@gmail.com>
2026-07-27 16:12:11 +0200

    Expose Blobben puddle edge inside card ring

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ import './subpage-theme.css'
 import busfahrerGameImage from './assets/spielbild icons/blobb-fahrer-cover-clean.png'
 import blobbenGameImage from './assets/spielbild icons/blobben-cover-light-transparent.png'
 import busfahrerGameImageNeon from './assets/spielbild icons/blobb-fahrer-cover-neon-chroma-v4.png'
-import blobbenGameImageNeon from './assets/spielbild icons/blobben-cover-neon-precise-ring-v15.png'
+import blobbenGameImageNeon from './assets/spielbild icons/blobben-cover-neon-smaller-puddle-v16.png'
 import heroLogo from './assets/überschrift/blobba-logo-clean-outlined.png'
 import heroLogoNeon from './assets/überschrift/blobba-logo-neon-original.png'
 
